ed5ac748ac0ed8e6d9f7183ec7e8fc025bfa1e6e
Nishank Tyagi <nishanktyagi7@gmail.com>
2024-07-26 01:40:53 +0530

    update build move file

--- build-and-move.mjs
+++ build-and-move.mjs
@@ -1,5 +1,5 @@
 import pkg from 'fs-extra';
-const { emptyDirSync, ensureDirSync, copySync, removeSync } = pkg;
+const { emptyDirSync, ensureDirSync, copySync, removeSync, readdirSync } = pkg;
 import { resolve, dirname } from 'path';
 import { exec } from 'child_process';
 import { fileURLToPath } from 'url';
@@ -9,6 +9,26 @@ const __dirname = dirname(__filename);
 
 const tempBuildDir = resolve(__dirname, 'temp-build');
 const destDir = resolve('C:/Users/HP/Documents/ccm-build');
+const gitDir = resolve(destDir, '.git');
+
+// Function to copy files excluding the .git directory
+const copyFilesExcludingGit = (sourceDir, targetDir) => {
+  const items = readdirSync(sourceDir, { withFileTypes: true });
+
+  items.forEach(item => {
+    const srcPath = resolve(sourceDir, item.name);
+    const destPath = resolve(targetDir, item.name);
+
+    if (item.isDirectory()) {
+      if (item.name !== '.git') { // Skip copying the .git directory
+        ensureDirSync(destPath);
+        copyFilesExcludingGit(srcPath, destPath);
+      }
+    } else {
+      pkg.copyFileSync(srcPath, destPath);
+    }
+  });
+};
 
 emptyDirSync(tempBuildDir);
 
@@ -22,9 +42,15 @@ exec('vite build', (error, stdout, stderr) => {
   console.error(stderr);
 
   ensureDirSync(destDir);
-  emptyDirSync(destDir);
-  copySync(tempBuildDir, destDir);
-  removeSync(tempBuildDir);
+  
+  // Ensure .git directory is preserved
+  if (!pkg.existsSync(gitDir)) {
+    copyFilesExcludingGit(tempBuildDir, destDir);
+  } else {
+    copyFilesExcludingGit(tempBuildDir, destDir);
+    // Remove temp build dir after copying
+    removeSync(tempBuildDir);
+  }
 
   console.log(`Build moved to ${destDir}`);
 });
